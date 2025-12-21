b4a6b79feaf72078fe03e2130c134a14a74e732c
Keshav Bisht <122655921+Keshavbisht@users.noreply.github.com>
2025-12-21 17:16:51 +0530

    Change DEBUG setting to use environment variable

--- DjangoServer/Backend/back/settings.py
+++ DjangoServer/Backend/back/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-50wg9zc$c-7qq5iv1=ukamf$f3pn65ruroj16vg(=5+_)@v)16"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ['*']
 
